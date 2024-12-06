c1840e5288c41546f43c4cd69ad231ab37433d36
Abdelaziz <ab.boulmal@gmail.com>
2024-12-06 09:38:09 +0100

    MongoDB Atlas & Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,33 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+const workoutRoutes = require('./routes/workouts');
 
-
-const workoutRoutes = require('./routes/workouts')
 // express app
-const app = express()
+const app = express();
 
 // middleware
-app.use(express.json())
+app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
 // routes
-app.use('/api.workouts',workoutRoutes)
+app.use('/api/workouts', workoutRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log('listening on port', process.env.PORT)
-})
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('connected to database');
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
